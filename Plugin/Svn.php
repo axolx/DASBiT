6a936bd02badbf235301dd8d648e920d926dcaaa
dasprid@07b5d564-ebd0-481b-8db0-eb002ad754c2 <none@none>
2009-07-26 22:46:47 +0000

    Finished SVN plugin
    --HG--
    extra : convert_revision : svn%3A07b5d564-ebd0-481b-8db0-eb002ad754c2/trunk%4040

--- Plugin/Svn.php
+++ Plugin/Svn.php
@@ -25,11 +25,11 @@
 class Plugin_Svn extends DASBiT_Plugin
 {
     /**
-     * Latest reported revision
+     * Database adapter
      *
-     * @var integer
+     * @var Zend_Db_Adapter_Pdo_Sqlite
      */
-    protected $_latestRevision;
+    protected $_adapter;
     
     /**
      * Defined by DASBiT_Plugin
@@ -38,13 +38,118 @@ class Plugin_Svn extends DASBiT_Plugin
      */
     protected function _init()
     {
-        $latestCommit = $this->_getCommits('HEAD');
+        $this->_adapter = DASBiT_Database::accessDatabase('svn', array(
+            'repositories' => array(
+                'repos_id'            => 'INTEGER PRIMARY KEY',
+                'repos_last_revision' => 'NUMBER',
+                'repos_channel'       => 'VARCHAR(40)',
+                'repos_url'           => 'VARCHAR(255)',
+                'repos_info_url'      => 'VARCHAR(255)'
+            )
+        ));
         
-        if (count($latestCommit) > 0) { 
-            $this->_latestRevision = $latestCommit[0]['revision'];
+        $select = $this->_adapter
+                       ->select()
+                       ->from('repositories',
+                              array('repos_id',
+                                    'repos_url'));
+                              
+        $repositories = $this->_adapter->fetchAll($select);
+        
+        foreach ($repositories as $repository) {
+            $latestCommit = $this->_getCommits($repository['repos_url'], 'HEAD');
+
+            if (count($latestCommit) > 0) {
+                $lastRevision = $latestCommit[0]['revision'];
+            } else {
+                $lastRevision = -1;
+            }
+            
+            $this->_adapter->update('repositories',
+                                    array('repos_last_revision' => $lastRevision),
+                                    'repos_id = ' . ($repository['repos_id'])); 
+        }
+
+        $this->_controller->registerCommand($this, 'add', 'svn add');
+        $this->_controller->registerCommand($this, 'remove', 'svn remove');
+        $this->_controller->registerInterval($this, 'watchUpdates', 120);
+    }
+    
+    /**
+     * Add SVN repository
+     *
+     * @param  DASBiT_Irc_Request $request
+     * @return void
+     */
+    public function add(DASBiT_Irc_Request $request)
+    {
+        if (!Plugin_Users::isIdentified($request)) {
+            $this->_client->send('You must be identified to add a repository', $request, DASBiT_Irc_Client::TYPE_NOTICE);
+            return;
         }
         
-        $this->_controller->registerInterval($this, 'watchUpdates', 120);
+        $words = array_slice($request->getWords(), 2);
+
+        if (count($words) < 2) {
+            $this->_client->send('Not enough arguments, channel and url required', $request, DASBiT_Irc_Client::TYPE_NOTICE);
+            return;
+        }
+        
+        $infoUrl      = (count($words) === 3) ? $words[2] : '';
+        $latestCommit = $this->_getCommits($words[1], 'HEAD');
+        
+        if (count($latestCommit) === 0) {
+            $this->_client->send('Could not access SVN repository', $request, DASBiT_Irc_Client::TYPE_NOTICE);
+            return;
+        }
+        
+        $this->_adapter->insert('repositories', array(
+            'repos_channel'       => $words[0],
+            'repos_url'           => $words[1],
+            'repos_info_url'      => $infoUrl,
+            'repos_last_revision' => $latestCommit[0]['revision']
+        ));
+        
+        $this->_client->send('Repository added', $request, DASBiT_Irc_Client::TYPE_NOTICE);
+    }
+
+    /**
+     * Remove SVN repository
+     *
+     * @param  DASBiT_Irc_Request $request
+     * @return void
+     */
+    public function remove(DASBiT_Irc_Request $request)
+    {
+        if (!Plugin_Users::isIdentified($request)) {
+            $this->_client->send('You must be identified to remove a repository', $request, DASBiT_Irc_Client::TYPE_NOTICE);
+            return;
+        }
+        
+        $words = array_slice($request->getWords(), 2);
+        
+        if (count($words) < 2) {
+            $this->_client->send('Not enough arguments, channel and url required', $request, DASBiT_Irc_Client::TYPE_NOTICE);
+            return;
+        }
+        
+        $select = $this->_adapter
+                       ->select()
+                       ->from('repositories',
+                              array('repos_id'))
+                       ->where('repos_channel = ?', $words[0])
+                       ->where('repos_url = ?', $words[1]);
+                       
+        $repository = $this->_adapter->fetchRow($select);
+        
+        if ($repository === false) {
+            $this->_client->send('There is no repository with this channel and URL', $request, DASBiT_Irc_Client::TYPE_NOTICE);
+            return;
+        }
+        
+        $this->_adapter->delete('repositories', $this->_adapter->quoteInto('repos_id = ?', $repository['repos_id']));
+        
+        $this->_client->send('Repository removed', $request, DASBiT_Irc_Client::TYPE_NOTICE);
     }
     
     /**
@@ -54,40 +159,59 @@ class Plugin_Svn extends DASBiT_Plugin
      */
     public function watchUpdates()
     {
-        $client  = new Zend_Http_Client();
-        $commits = $this->_getCommits(($this->_latestRevision + 1) . ':HEAD');
+        $select = $this->_adapter
+                       ->select()
+                       ->from('repositories',
+                              array('repos_id',
+                                    'repos_url',
+                                    'repos_info_url',
+                                    'repos_last_revision'))
+                       ->where('repos_last_revision >= 0');
+                              
+        $repositories = $this->_adapter->fetchAll($select);
+        $client       = new Zend_Http_Client();
         
-        foreach ($commits as $commit) {
-            $url = 'http://framework.zend.com/code/changelog/Zend_Framework/?cs=' . $commit['revision'];
+        foreach ($repositories as $repository) {  
+            $commits = $this->_getCommits($repository['repos_url'], ($repository['repos_last_revision'] + 1) . ':HEAD');
             
-            $client->setUri('http://tinyurl.com/api-create.php?url=' . $url);
-            $response = $client->request();
-            
-            if ($response->isSuccessful()) {
-                $url = $response->getBody();   
+            foreach ($commits as $commit) {
+                $response = sprintf('[SVN:r%d:%s] %s',
+                                    $commit['revision'],
+                                    $commit['username'],
+                                    $commit['message']);
+                                    
+                if (!empty($repository['repos_info_url'])) {
+                    $url = str_replace('%r', $commit['revision'], $repository['repos_info_url']);
+                    
+                    $client->setUri('http://tinyurl.com/api-create.php?url=' . $url);
+                    $response = $client->request();
+                    
+                    if ($response->isSuccessful()) {
+                        $url = $response->getBody();   
+                    }
+                    
+                    $response .= sprintf(' (See: %s)', $url);
+                }
+    
+                $this->_client->send($response, $request);
+                
+                $this->_adapter->update('repositories',
+                                        array('repos_last_revision' => $commit['revision']),
+                                        'repos_id = ' . ($repository['repos_id'])); 
             }
-            
-            $response = sprintf('[SVN:r%d:%s] %s (See: %s)',
-                                $commit['revision'],
-                                $commit['username'],
-                                $commit['message'],
-                                $url);
-
-            $this->_client->send($response, $request);
-            
-            $this->_latestRevision = $commit['revision'];
         }
     }
     
     /**
      * Get commits according to the given range
      *
+     * @param  string $url
      * @param  string $range
      * @return array
      */
-    protected function _getCommits($range)
+    protected function _getCommits($url, $range)
     {
-        $logResults = explode("\n", shell_exec('svn log -r' . $range . ' http://framework.zend.com/svn/framework'));
+        $logResults = explode("\n", shell_exec('svn log --non-interactive -r' . $range . ' ' . $url));
         $commits    = array();
 
         foreach ($logResults as $totalLineNum => $content) {
