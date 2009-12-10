a1465504bfd813ed6d21252412c7caac1c422adc
dasprid@07b5d564-ebd0-481b-8db0-eb002ad754c2 <none@none>
2009-12-10 15:55:09 +0000

    Added manual search
    --HG--
    extra : convert_revision : svn%3A07b5d564-ebd0-481b-8db0-eb002ad754c2/trunk%4055

---
+++ Plugin/Manual.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * DASBiT
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @version $Id: Factoids.php 47 2009-08-22 20:06:01Z dasprid $
+ */
+
+/**
+ * Plugin to search the ZF manual
+ */
+class Plugin_Manual extends DASBiT_Plugin
+{
+    /**
+     * Defined by DASBiT_Plugin
+     *
+     * @return void
+     */
+    protected function _init()
+    {       
+        $this->_controller->registerCommand($this, 'search', 'manual');
+        $this->_controller->registerCommand($this, 'search', 'm');
+    }
+       
+    /**
+     * Search the manual and return the first result
+     *
+     * @param  DASBiT_Irc_Request $request
+     * @return void
+     */
+    public function search(DASBiT_Irc_Request $request)
+    {
+        $searchTerms = implode(' ', array_slice($request->getWords(), 1));
+
+        $client = new Zend_Http_Client();
+        $client->setParameterGet('v', '1.0')
+               ->setParameterGet('q', $searchTerms . ' site:http://framework.zend.com/manual/en/')
+               ->setUri('http://ajax.googleapis.com/ajax/services/search/web')
+               ->setHeaders('Referer', 'http://dasbit.dasprids.de');
+
+        $result = $client->request();
+
+        if (!$result->isSuccessful()) {
+            $this->_client->send('An error occured while querying the search engine', $request, DASBiT_Irc_Client::TYPE_NOTICE);
+        }
+
+        $data = json_decode($result->getBody());
+
+        if ($data === null) {
+            $this->_client->send('An error occured while processing the result', $request, DASBiT_Irc_Client::TYPE_NOTICE);
+        } elseif (!isset($data->responseData->results[0])) {
+            $this->_client->send('Nothing found', $request);
+        }
+
+        $result = $data->responseData->results[0];
+
+        if (preg_match('(^(.*) - Zend Framework: Documentation$)', '\1', $result->title, $matches)) {
+            $description = strip_tags($matches[0]) . ' - ';
+        } else {
+            $description = '';
+        }
+
+        $description .= strip_tags($result->content);
+
+        $client  = new Zend_Http_Client(sprintf('http://tinyurl.com/api-create.php?url=%s', urlencode($result->url)));
+        $tinyUrl = $client->request()->getBody();
+
+        $this->_client->send($description . '; ' . $tinyUrl, $request);
+    }
+}
